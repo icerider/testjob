bd1561d25e0efccd8432cb7f5849f986c1b9b68b
Хирецкий Михаил <mh@calculate.ru>
2021-09-13 10:00:25 +0300

    Добавлен README.md

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd app
-SQLALCHEMY_DATABASE_URI=sqlite+aiosqlite:///work.db ../env/bin/uvicorn main:app
+SQLALCHEMY_DATABASE_URI=${SQLALCHEMY_DATABASE_URI:-sqlite+aiosqlite:///work.db} ../env/bin/uvicorn main:app
